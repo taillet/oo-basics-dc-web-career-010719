c0eda9b091aa689d3d5339de553544719ef9f11f
Heloise Taillet <ht2394@columbia.edu>
2019-01-09 19:20:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,7 @@ class Shoe
   attr_accessor :color, :size, :material
   attr_reader :brand, :condition
   
-  def initialize(brand, condition)
+  def initialize(brand)
     @brand = brand
   end
     
@@ -13,13 +13,9 @@ class Shoe
     @brand
   end
   
-  def condition
-    @condition
-  end
   
   def cobble
     puts "Your shoe is as good as new!"
-    condition = mew
   end
 end
 
