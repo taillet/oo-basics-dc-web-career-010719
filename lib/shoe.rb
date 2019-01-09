fff23481f9de6d3ee28f812916f955e86dd8d3f8
Heloise Taillet <ht2394@columbia.edu>
2019-01-09 19:18:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,10 +2,18 @@
 
 class Shoe
   
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :size, :material
   attr_reader :brand
   
   def initialize(brand)
+    @brand = brand
+  end
+    
+  def brand
+    @brand
+  end
+  
+    def initialize(condition)
     @brand = brand
   end
     
