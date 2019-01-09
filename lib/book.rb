738c4620649cff930daf83b26c3adb6a3579aace
Heloise Taillet <ht2394@columbia.edu>
2019-01-09 19:07:32 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,14 +1,14 @@
 class Book
   
-    def initialize(title)
-      @title = title
-    end
+  def initialize(title)
+    @title = title
+  end
     
-    def title
-      @title
-    end
+  def title
+    @title
+  end
     
-      def author=(author)
+  def author=(author)
     @author = author
   end
 end
