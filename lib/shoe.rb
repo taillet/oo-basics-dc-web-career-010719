5f620d9f009cea69fe9a4b525936f8c94c519466
Heloise Taillet <ht2394@columbia.edu>
2019-01-09 19:19:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,13 +14,6 @@ class Shoe
     @brand
   end
   
-    def initialize(condition)
-    @condition = condition
-  end
-    
-  def condition
-    @condition
-  end
   
   def cobble
     puts "Your shoe is as good as new!"
